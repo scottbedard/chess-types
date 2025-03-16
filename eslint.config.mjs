a820c3d9a066bd939a4fd2c5bcd15e8737fd6bd6
Scott Bedard <scottbedard90@gmail.com>
2025-03-16 14:57:01 -0500

    style preferences

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import eslint from '@eslint/js';
+import eslint from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
-import tseslint from 'typescript-eslint';
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -12,7 +12,12 @@ export default tseslint.config(
     },
     rules: {
       '@stylistic/eol-last': ['error', 'always'],
-      '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^_' }]
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^_' }],
     }
   }
 )
