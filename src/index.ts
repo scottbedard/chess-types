205dc4eefd0704ba3650b2396b93de9e953e75da
Scott Bedard <scottbedard90@gmail.com>
2025-03-18 19:46:34 -0500

    display

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './base'
+export * from './board'
+export * from './game'
+export * from './notation'
+export * from './utils'
--- src/utils.ts
+++ src/utils.ts
@@ -30,6 +30,9 @@ export type IsLength<
     ? true
     : false
 
+/** Test if a number is odd */
+export type IsOdd<T extends number> = `${T}` extends `${string}${'1' | '3' | '5' | '7' | '9'}` ? true : false
+
 /** Map a tuple of indices to san notation */
 export type ToMoves<
   T extends Index[],
