b0156c80359b73669f610b8b1f26f51a6569181f
Scott Bedard <scottbedard90@gmail.com>
2025-03-18 21:53:14 -0500

    typo

--- src/game.ts
+++ src/game.ts
@@ -6,7 +6,7 @@ export type _DisplayGame<T extends ParsedGame> = {
   7: [` ${_Dark<T, 8>} ${_Dark<T, 9>} ${_Dark<T, 10>} ${_Dark<T, 11>} ${_Dark<T, 12>} ${_Dark<T, 13>} ${_Dark<T, 14>} ${_Dark<T, 15>} `],
   6: [` ${_Light<T, 16>} ${_Light<T, 17>} ${_Light<T, 18>} ${_Light<T, 19>} ${_Light<T, 20>} ${_Light<T, 21>} ${_Light<T, 22>} ${_Light<T, 23>} `],
   5: [` ${_Dark<T, 24>} ${_Dark<T, 25>} ${_Dark<T, 26>} ${_Dark<T, 27>} ${_Dark<T, 28>} ${_Dark<T, 29>} ${_Dark<T, 30>} ${_Dark<T, 31>} `],
-  4: [` {_Light<T, 32>} ${_Light<T, 33>} ${_Light<T, 34>} ${_Light<T, 35>} ${_Light<T, 36>} ${_Light<T, 37>} ${_Light<T, 38>} ${_Light<T, 39>} `],
+  4: [` ${_Light<T, 32>} ${_Light<T, 33>} ${_Light<T, 34>} ${_Light<T, 35>} ${_Light<T, 36>} ${_Light<T, 37>} ${_Light<T, 38>} ${_Light<T, 39>} `],
   3: [` ${_Dark<T, 40>} ${_Dark<T, 41>} ${_Dark<T, 42>} ${_Dark<T, 43>} ${_Dark<T, 44>} ${_Dark<T, 45>} ${_Dark<T, 46>} ${_Dark<T, 47>} `],
   2: [` ${_Light<T, 48>} ${_Light<T, 49>} ${_Light<T, 50>} ${_Light<T, 51>} ${_Light<T, 52>} ${_Light<T, 53>} ${_Light<T, 54>} ${_Light<T, 55>} `],
   1: [` ${_Dark<T, 56>} ${_Dark<T, 57>} ${_Dark<T, 58>} ${_Dark<T, 59>} ${_Dark<T, 60>} ${_Dark<T, 61>} ${_Dark<T, 62>} ${_Dark<T, 63>} `],
