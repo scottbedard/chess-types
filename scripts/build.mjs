382ea49755ec2600c001b519da6bfe4a82cd8311
Scott Bedard <scottbedard90@gmail.com>
2025-03-19 21:47:59 -0500

    remove rollup

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,7 +2,7 @@ import { dirname, resolve } from 'node:path'
 import { existsSync, readFileSync, renameSync, rmSync, writeFileSync } from 'node:fs'
 import { fileURLToPath } from 'node:url'
 
-const dir = dirname(fileURLToPath(import.meta.url))
+const dir = resolve(dirname(fileURLToPath(import.meta.url)), '..')
 const build = resolve(dir, 'build')
 const dist = resolve(dir, 'dist')
 const index = resolve(dist, 'index.d.ts')
@@ -21,5 +21,3 @@ writeFileSync(index, readFileSync(index, 'utf8').replace('x.y.z', pkg.version))
 writeFileSync(resolve(dist, 'index.mjs'), `const pkg = '@bedard/chess-types - ${pkg.version}';\nexport { pkg };\n`)
 
 cleanup()
-
-console.log('\x1b[32m','Done')
