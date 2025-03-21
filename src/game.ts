ef05d855ae3283abc661e393e7a509d20a470285
Scott Bedard <scottbedard90@gmail.com>
2025-03-20 20:58:58 -0500

    support flipped board

--- src/game.ts
+++ src/game.ts
@@ -22,26 +22,9 @@ import type { QueenMoves } from './pieces/queen'
 import type { RookMoves } from './pieces/rook'
 import type { ToPositions, ToSans } from './notation'
 
-/** Get all positions occupied by a color */
-export type OccupiedBy<
-  Game extends ParsedGame,
-  C extends Color,
-  Remaining extends Index[] = Indices,
-  Acc extends Index[] = []
-> = ToPositions<_OccupiedBy<Game, C, Remaining, Acc>>
-
-export type _OccupiedBy<
-  Game extends ParsedGame,
-  C extends Color,
-  Remaining extends Index[] = Indices,
-  Acc extends Index[] = []
-> = Remaining extends [infer Head extends Index, ...infer Tail extends Index[]]
-  ? Game['board'][Head] extends FriendlyPiece<C>
-    ? _OccupiedBy<Game, C, Tail, [...Acc, Head]>
-    : _OccupiedBy<Game, C, Tail, Acc>
-  : Acc
-
-/** Get all possible moves, even ones that result in self-check */
+/**
+ * Get all possible moves, even ones that result in self-check
+ **/
 export type CurrentMovesUnsafe<
   Game extends ParsedGame,
   Turn extends Color = Game['turn'],
@@ -68,29 +51,48 @@ export type _CurrentMovesUnsafe<
     : never
   : Acc
 
-/** create a debuggable representation of the chessboard */
-export type Chessboard<T extends ParsedGame> = {
-  8: ` ${_Square<T,  0, _White>} ${_Square<T,  1, _Black>} ${_Square<T,  2, _White>} ${_Square<T,  3, _Black>} ${_Square<T,  4, _White>} ${_Square<T,  5, _Black>} ${_Square<T,  6, _White>} ${_Square<T,  7, _Black>} `,
-  7: ` ${_Square<T,  8, _Black>} ${_Square<T,  9, _White>} ${_Square<T, 10, _Black>} ${_Square<T, 11, _White>} ${_Square<T, 12, _Black>} ${_Square<T, 13, _White>} ${_Square<T, 14, _Black>} ${_Square<T, 15, _White>} `,
-  6: ` ${_Square<T, 16, _White>} ${_Square<T, 17, _Black>} ${_Square<T, 18, _White>} ${_Square<T, 19, _Black>} ${_Square<T, 20, _White>} ${_Square<T, 21, _Black>} ${_Square<T, 22, _White>} ${_Square<T, 23, _Black>} `,
-  5: ` ${_Square<T, 24, _Black>} ${_Square<T, 25, _White>} ${_Square<T, 26, _Black>} ${_Square<T, 27, _White>} ${_Square<T, 28, _Black>} ${_Square<T, 29, _White>} ${_Square<T, 30, _Black>} ${_Square<T, 31, _White>} `,
-  4: ` ${_Square<T, 32, _White>} ${_Square<T, 33, _Black>} ${_Square<T, 34, _White>} ${_Square<T, 35, _Black>} ${_Square<T, 36, _White>} ${_Square<T, 37, _Black>} ${_Square<T, 38, _White>} ${_Square<T, 39, _Black>} `,
-  3: ` ${_Square<T, 40, _Black>} ${_Square<T, 41, _White>} ${_Square<T, 42, _Black>} ${_Square<T, 43, _White>} ${_Square<T, 44, _Black>} ${_Square<T, 45, _White>} ${_Square<T, 46, _Black>} ${_Square<T, 47, _White>} `,
-  2: ` ${_Square<T, 48, _White>} ${_Square<T, 49, _Black>} ${_Square<T, 50, _White>} ${_Square<T, 51, _Black>} ${_Square<T, 52, _White>} ${_Square<T, 53, _Black>} ${_Square<T, 54, _White>} ${_Square<T, 55, _Black>} `,
-  1: ` ${_Square<T, 56, _Black>} ${_Square<T, 57, _White>} ${_Square<T, 58, _Black>} ${_Square<T, 59, _White>} ${_Square<T, 60, _Black>} ${_Square<T, 61, _White>} ${_Square<T, 62, _Black>} ${_Square<T, 63, _White>} `,
-}
+/**
+ * Create a hover-display of the chessboard
+ **/
+export type Chessboard<
+  T extends ParsedGame,
+  Flipped extends boolean = false
+> =
+  Flipped extends true
+    ? {
+      8: ` ${_Square<T, 63, _W>} ${_Square<T, 62, _B>} ${_Square<T, 61, _W>} ${_Square<T, 60, _B>} ${_Square<T, 59, _W>} ${_Square<T, 58, _B>} ${_Square<T, 57, _W>} ${_Square<T, 56, _B>} `,
+      7: ` ${_Square<T, 55, _B>} ${_Square<T, 54, _W>} ${_Square<T, 53, _B>} ${_Square<T, 52, _W>} ${_Square<T, 51, _B>} ${_Square<T, 50, _W>} ${_Square<T, 49, _B>} ${_Square<T, 48, _W>} `,
+      6: ` ${_Square<T, 47, _W>} ${_Square<T, 46, _B>} ${_Square<T, 45, _W>} ${_Square<T, 44, _B>} ${_Square<T, 43, _W>} ${_Square<T, 42, _B>} ${_Square<T, 41, _W>} ${_Square<T, 40, _B>} `,
+      5: ` ${_Square<T, 39, _B>} ${_Square<T, 38, _W>} ${_Square<T, 37, _B>} ${_Square<T, 36, _W>} ${_Square<T, 35, _B>} ${_Square<T, 34, _W>} ${_Square<T, 33, _B>} ${_Square<T, 32, _W>} `,
+      4: ` ${_Square<T, 31, _W>} ${_Square<T, 30, _B>} ${_Square<T, 29, _W>} ${_Square<T, 28, _B>} ${_Square<T, 27, _W>} ${_Square<T, 26, _B>} ${_Square<T, 25, _W>} ${_Square<T, 24, _B>} `,
+      3: ` ${_Square<T, 23, _B>} ${_Square<T, 22, _W>} ${_Square<T, 21, _B>} ${_Square<T, 20, _W>} ${_Square<T, 19, _B>} ${_Square<T, 18, _W>} ${_Square<T, 17, _B>} ${_Square<T, 16, _W>} `,
+      2: ` ${_Square<T, 15, _W>} ${_Square<T, 14, _B>} ${_Square<T, 13, _W>} ${_Square<T, 12, _B>} ${_Square<T, 11, _W>} ${_Square<T, 10, _B>} ${_Square<T,  9, _W>} ${_Square<T,  8, _B>} `,
+      1: ` ${_Square<T,  7, _B>} ${_Square<T,  6, _W>} ${_Square<T,  5, _B>} ${_Square<T,  4, _W>} ${_Square<T,  3, _B>} ${_Square<T,  2, _W>} ${_Square<T,  1, _B>} ${_Square<T,  0, _W>} `,
+    }
+    : {
+      8: ` ${_Square<T,  0, _W>} ${_Square<T,  1, _B>} ${_Square<T,  2, _W>} ${_Square<T,  3, _B>} ${_Square<T,  4, _W>} ${_Square<T,  5, _B>} ${_Square<T,  6, _W>} ${_Square<T,  7, _B>} `,
+      7: ` ${_Square<T,  8, _B>} ${_Square<T,  9, _W>} ${_Square<T, 10, _B>} ${_Square<T, 11, _W>} ${_Square<T, 12, _B>} ${_Square<T, 13, _W>} ${_Square<T, 14, _B>} ${_Square<T, 15, _W>} `,
+      6: ` ${_Square<T, 16, _W>} ${_Square<T, 17, _B>} ${_Square<T, 18, _W>} ${_Square<T, 19, _B>} ${_Square<T, 20, _W>} ${_Square<T, 21, _B>} ${_Square<T, 22, _W>} ${_Square<T, 23, _B>} `,
+      5: ` ${_Square<T, 24, _B>} ${_Square<T, 25, _W>} ${_Square<T, 26, _B>} ${_Square<T, 27, _W>} ${_Square<T, 28, _B>} ${_Square<T, 29, _W>} ${_Square<T, 30, _B>} ${_Square<T, 31, _W>} `,
+      4: ` ${_Square<T, 32, _W>} ${_Square<T, 33, _B>} ${_Square<T, 34, _W>} ${_Square<T, 35, _B>} ${_Square<T, 36, _W>} ${_Square<T, 37, _B>} ${_Square<T, 38, _W>} ${_Square<T, 39, _B>} `,
+      3: ` ${_Square<T, 40, _B>} ${_Square<T, 41, _W>} ${_Square<T, 42, _B>} ${_Square<T, 43, _W>} ${_Square<T, 44, _B>} ${_Square<T, 45, _W>} ${_Square<T, 46, _B>} ${_Square<T, 47, _W>} `,
+      2: ` ${_Square<T, 48, _W>} ${_Square<T, 49, _B>} ${_Square<T, 50, _W>} ${_Square<T, 51, _B>} ${_Square<T, 52, _W>} ${_Square<T, 53, _B>} ${_Square<T, 54, _W>} ${_Square<T, 55, _B>} `,
+      1: ` ${_Square<T, 56, _B>} ${_Square<T, 57, _W>} ${_Square<T, 58, _B>} ${_Square<T, 59, _W>} ${_Square<T, 60, _B>} ${_Square<T, 61, _W>} ${_Square<T, 62, _B>} ${_Square<T, 63, _W>} `,
+    }
 
-type _Black = '*'
+type _B = '*'
 
-type _White = '-'
+type _W = '-'
 
 type _Square<
   Game extends ParsedGame,
   I extends Index,
-  Char extends _Black | _White
+  Char extends _B | _W
 > = Game['board'][I] extends infer P extends Piece ? P : Char
 
-/** Find king by color */
+/**
+ * Find king by color
+ */
 export type FindKing<
   Game extends ParsedGame,
   C extends Color,
@@ -111,7 +113,9 @@ export type _FindKing<
     : _FindKing<Game, C, King, Tail>
   : false
 
-/** Test if a king is threatened */
+/**
+ * Test if a king is threatened
+ **/
 export type IsCheck<
   Game extends ParsedGame,
   KingColor extends Color = Game['turn'],
@@ -119,7 +123,9 @@ export type IsCheck<
   ? _IsThreatened<Game, KingIndex, EnemyColor<KingColor>>
   : false
 
-/** Test if position is threatened by a hostile color */
+/**
+ * Test if position is threatened by a hostile color
+ **/
 export type IsThreatened<
   Game extends ParsedGame,
   Target extends Position,
@@ -150,3 +156,24 @@ type _IsReachable<
     ? true
     : _IsReachable<Target, Tail>
   : false
+
+/**
+ * Get all positions occupied by a color
+ **/
+export type OccupiedBy<
+  Game extends ParsedGame,
+  C extends Color,
+  Remaining extends Index[] = Indices,
+  Acc extends Index[] = []
+> = ToPositions<_OccupiedBy<Game, C, Remaining, Acc>>
+
+export type _OccupiedBy<
+  Game extends ParsedGame,
+  C extends Color,
+  Remaining extends Index[] = Indices,
+  Acc extends Index[] = []
+> = Remaining extends [infer Head extends Index, ...infer Tail extends Index[]]
+  ? Game['board'][Head] extends FriendlyPiece<C>
+    ? _OccupiedBy<Game, C, Tail, [...Acc, Head]>
+    : _OccupiedBy<Game, C, Tail, Acc>
+  : Acc
--- tests/game.test-d.ts
+++ tests/game.test-d.ts
@@ -14,9 +14,22 @@ describe('Chessboard', () => {
   test('empty board', () => {
     type Game = ParseFen<'8/8/8/8/8/8/8/8 w KQkq - 0 1'>
 
-    type Result = Chessboard<Game>
+    type White = Chessboard<Game>
 
-    assertType<Result>({
+    type Black = Chessboard<Game, true>
+
+    assertType<White>({
+      8: ' - * - * - * - * ',
+      7: ' * - * - * - * - ',
+      6: ' - * - * - * - * ',
+      5: ' * - * - * - * - ',
+      4: ' - * - * - * - * ',
+      3: ' * - * - * - * - ',
+      2: ' - * - * - * - * ',
+      1: ' * - * - * - * - ',
+    })
+
+    assertType<Black>({
       8: ' - * - * - * - * ',
       7: ' * - * - * - * - ',
       6: ' - * - * - * - * ',
@@ -31,9 +44,11 @@ describe('Chessboard', () => {
   test('starting position', () => {
     type Game = ParseFen<'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'>
 
-    type Result = Chessboard<Game>
+    type White = Chessboard<Game>
 
-    assertType<Result>({
+    type Black = Chessboard<Game, true>
+
+    assertType<White>({
       8: ' r n b q k b n r ',
       7: ' p p p p p p p p ',
       6: ' - * - * - * - * ',
@@ -42,6 +57,17 @@ describe('Chessboard', () => {
       3: ' * - * - * - * - ',
       2: ' P P P P P P P P ',
       1: ' R N B Q K B N R ',
+    })
+
+    assertType<Black>({
+        8: ' R N B K Q B N R ',
+        7: ' P P P P P P P P ',
+        6: ' - * - * - * - * ',
+        5: ' * - * - * - * - ',
+        4: ' - * - * - * - * ',
+        3: ' * - * - * - * - ',
+        2: ' p p p p p p p p ',
+        1: ' r n b k q b n r ',
     })
   })
 })
