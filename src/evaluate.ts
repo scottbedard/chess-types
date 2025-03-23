34819c4ce10c8b28bae26f11507224f5aba50e7c
Scott Bedard <scottbedard90@gmail.com>
2025-03-23 00:07:52 -0500

    wip

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -7,23 +7,47 @@ import type {
   Piece,
 } from './base'
 
-import type { _ApplyMoveUnsafe, _CurrentMoves } from './game'
+import type {
+  _ApplyMoveUnsafe,
+  _CurrentMoves,
+  Chessboard,
+  NewGame
+} from './game'
+
+import type {
+  FormatGame,
+  FormatSan,
+  ParseFen
+} from './notation'
 
 import type { Sum } from './utils'
 
 /**
  * Get the next move
  */
-export type _SearchLayer<
+type _Node = {
+  move: string
+  fen: string
+  score: number
+}
+
+type _Layer<
   Game extends ParsedGame,
-  Turn extends Color = Game['turn'],
-  Moves extends ParsedMove[] = _CurrentMoves<Game, Turn>,
-  Result extends number[] = []
+  Moves extends ParsedMove[] = _CurrentMoves<Game>,
+  Acc extends _Node[] = []
 > = Moves extends [infer Head extends ParsedMove, ...infer Tail extends ParsedMove[]]
-  ? _ApplyMoveUnsafe<Game, Head> extends infer NextGame extends ParsedGame
-    ? _SearchLayer<NextGame, Turn, Tail, [Evaluate<NextGame>, ...Result]>
-    : 1
-  : Result
+  ? _Layer<Game, Tail, [...Acc, {
+    move: FormatSan<Head>,
+    fen: FormatGame<_ApplyMoveUnsafe<Game, Head>>
+    score: Evaluate<_ApplyMoveUnsafe<Game, Head>>
+  }]>
+  : Acc
+
+type Game = ParseFen<'rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2'>
+
+type Foo = _Layer<Game>
+
+type Output = Chessboard<Game>
 
 /**
  * Evaluate game state
@@ -37,6 +61,11 @@ export type Evaluate<
     ? Evaluate<Game, Sum<Value, _PieceValue[P]>, Tail>
     : Evaluate<Game, Value, Tail>
   : Value
+
+type _EvaluateMove<
+  Game extends ParsedGame,
+  Move extends ParsedMove,
+> = Evaluate<_ApplyMoveUnsafe<Game, Move>>
 
 type _PieceValue = {
   'k': -90
